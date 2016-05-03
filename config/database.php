48b71acfd9963105e30a3f5021d20a4e6eec6aa5
Jenhlab <vagrant@homestead>
2016-05-03 17:19:52 +0000

    database.php edit

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => getenv('DB_HOST'),
-            'database'  => getenv('DB_DATABASE'),
-            'username'  => getenv('DB_USERNAME'),
-            'password'  => getenv('DB_PASSWORD'),
+            'host'      => ('DB_HOST', 'localhost'),
+            'database'  => ('DB_DATABASE', 'forge'),
+            'username'  => ('DB_USERNAME', 'forge'),
+            'password'  => ('DB_PASSWORD', '2QxsnDt9ONvGxK1xjyzj'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
